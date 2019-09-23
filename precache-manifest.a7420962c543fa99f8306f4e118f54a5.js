53c8ca0c4f38b51d7a26f04aac8d23b1770d47f7
Volodsher <volodsher@gmail.com>
2019-09-23 10:03:19 +0300

    Updates

---
+++ precache-manifest.a7420962c543fa99f8306f4e118f54a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60ece649614aa502e4449bc00ceceed",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "4f9f6eb690896aa0f6ef",
+    "url": "/react_add-todo-form/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "a98bb21db1e89d8a2954",
+    "url": "/react_add-todo-form/static/js/2.d99334fc.chunk.js"
+  },
+  {
+    "revision": "4f9f6eb690896aa0f6ef",
+    "url": "/react_add-todo-form/static/js/main.597c6eb6.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
