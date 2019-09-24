a837ebecf6034c1e09bbc3a585f1d38b55f9a573
Volodsher <volodsher@gmail.com>
2019-09-24 16:35:20 +0300

    Updates

--- precache-manifest.4d0e70cc0d3c1168fc8edbd7677d5a89.js
+++ precache-manifest.4d0e70cc0d3c1168fc8edbd7677d5a89.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f60ece649614aa502e4449bc00ceceed",
+    "revision": "3fd14c38c68c131e7c24762b9fa80a81",
     "url": "/react_add-todo-form/index.html"
   },
   {
-    "revision": "4f9f6eb690896aa0f6ef",
+    "revision": "a3109870610a2945d3cd",
     "url": "/react_add-todo-form/static/css/main.cb9e985a.chunk.css"
   },
   {
-    "revision": "a98bb21db1e89d8a2954",
-    "url": "/react_add-todo-form/static/js/2.d99334fc.chunk.js"
+    "revision": "2878e5a452f7bd7800c2",
+    "url": "/react_add-todo-form/static/js/2.10ee9465.chunk.js"
   },
   {
-    "revision": "4f9f6eb690896aa0f6ef",
-    "url": "/react_add-todo-form/static/js/main.597c6eb6.chunk.js"
+    "revision": "a3109870610a2945d3cd",
+    "url": "/react_add-todo-form/static/js/main.164a0d44.chunk.js"
   },
   {
     "revision": "4953b3d381bec0cda918",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_add-todo-form/precache-manifest.a7420962c543fa99f8306f4e118f54a5.js"
+  "/react_add-todo-form/precache-manifest.4d0e70cc0d3c1168fc8edbd7677d5a89.js"
 );
 
 self.addEventListener('message', (event) => {
